8bee2cadfc587000215e229643fdfe9531d6e37a
Neagu Stefan <nstefanclaudel13@gmail.com>
2025-05-23 18:29:52 +0300

    Making the app work offline by adding special logic to the caching of the root file

--- public/sw.js
+++ public/sw.js
@@ -11,16 +11,37 @@ self.addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   // Only cache same-origin requests (static files, etc)
   if (url.origin === self.location.origin) {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request).then(fetchResponse => {
-          return caches.open('dynamic-v1').then(cache => {
-            cache.put(event.request, fetchResponse.clone());
-            return fetchResponse;
+    // Special logic for root URL ('/')
+    if (url.pathname === '/') {
+      event.respondWith(
+        fetch(event.request)
+          .then(response => {
+            // Update cache with latest index.html
+            return caches.open('dynamic-v1').then(cache => {
+              cache.put(event.request, response.clone());
+              return response;
+            });
+          })
+          .catch(() => {
+            // If fetch fails, return cached version if available
+            return caches.match(event.request);
+          })
+      );
+    }
+    else {
+      event.respondWith(
+        caches.match(event.request).then(response => {
+          return response || fetch(event.request).then(fetchResponse => {
+            return caches.open('dynamic-v1').then(cache => {
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            });
           });
-        });
-      })
-    );
+        })
+      );
+    }
   }
+
+  // and not the root URL (to avoid caching the index.html)
 });
 
