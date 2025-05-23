fec2e14c73bc43ab1c0003e7f514c3c3d5bcca56
Neagu Stefan <nstefanclaudel13@gmail.com>
2025-05-23 15:05:01 +0300

    Added caching for same-origin files

--- public/sw.js
+++ public/sw.js
@@ -7,3 +7,20 @@ self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  // Only cache same-origin requests (static files, etc)
+  if (url.origin === self.location.origin) {
+    event.respondWith(
+      caches.match(event.request).then(response => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open('dynamic-v1').then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+    );
+  }
+});
+
